e426b037d9ac8dcabc32f038b8e5ac8c5edfbdef
mymi14s <mymi14s@hotmail.com>
2022-06-12 22:14:21 +0100

    fix

--- ngnpayee/events/salary_slip.py
+++ ngnpayee/events/salary_slip.py
@@ -1,40 +1,40 @@
 import frappe ,json ,requests #line:1
 from frappe .utils import (add_days ,cint ,cstr ,date_diff ,flt ,formatdate ,get_first_day ,getdate ,money_in_words ,rounded )#line:5
 pinger =frappe .get_hooks (app_name ='ngnpayee',hook ='pinger')[0 ]#line:6
-def validate (O00O000OO0000O00O ,O0OOO0OOO00OO0000 ):#line:8
-	O0OO000OO0O000O0O =check_compute_payee (O00O000OO0000O00O )#line:9
-	if O0OO000OO0O000O0O .status :#line:10
-		O000O00OO000OOO00 =O0OO000OO0O000O0O .total #line:11
-		if ('PAYE'in [O0OO0O00OOOOO0O0O .salary_component for O0OO0O00OOOOO0O0O in O00O000OO0000O00O .deductions ]):#line:13
-			for O00000O0000OOOOO0 in O00O000OO0000O00O .deductions :#line:14
-				if (O00000O0000OOOOO0 .salary_component =='PAYE'):#line:15
-					O00000O0000OOOOO0 .amount =O000O00OO000OOO00 #line:16
+def validate (OOO000OOOO0OOO000 ,OO00O0O000OOOO00O ):#line:8
+	O00O000O0OOOO0OO0 =check_compute_payee (OOO000OOOO0OOO000 )#line:9
+	if O00O000O0OOOO0OO0 .status :#line:10
+		OOOOOOOOOO0OO0OOO =O00O000O0OOOO0OO0 .total #line:11
+		if ('PAYE'in [O0OO0O0OO0O0O0OOO .salary_component for O0OO0O0OO0O0O0OOO in OOO000OOOO0OOO000 .deductions ]):#line:13
+			for O0O00000000OO000O in OOO000OOOO0OOO000 .deductions :#line:14
+				if (O0O00000000OO000O .salary_component =='PAYE'):#line:15
+					O0O00000000OO000O .amount =OOOOOOOOOO0OO0OOO #line:16
 		else :#line:17
-			O00O000OO0000O00O .append ('deductions',{'abbr':"P",'amount':O000O00OO000OOO00 ,'salary_component':"PAYE"})#line:22
-		O00O000OO0000O00O .set_totals ()#line:23
-def calculate_values (OOOOOO000OOO0OOO0 ,OO000O0O00OOOO0OO ):#line:26
+			OOO000OOOO0OOO000 .append ('deductions',{'abbr':"P",'amount':OOOOOOOOOO0OO0OOO ,'salary_component':"PAYE"})#line:22
+		OOO000OOOO0OOO000 .set_totals ()#line:23
+def calculate_values (OOOOOO00000O000O0 ,OOOOO0O0O00O00O0O ):#line:26
 	""#line:29
 	try :#line:30
-		OOOOOO000OOO0OOO0 =OOOOOO000OOO0OOO0 .as_dict ()#line:31
-	except Exception as OOOOO0OO00OO00OOO :#line:32
+		OOOOOO00000O000O0 =OOOOOO00000O000O0 .as_dict ()#line:31
+	except Exception as O0000000OOOOO0O00 :#line:32
 		pass #line:33
-	OOOO0OOOOO0OOOOO0 =[OO000OO0O00O00O0O .component for OO000OO0O00O00O0O in OO000O0O00OOOO0OO .non_taxable_deductions ]#line:35
-	OO000OOO00O0OO00O =requests .post (pinger ,json ={'doc':OOOOOO000OOO0OOO0 ,'non_taxable_keys':json .dumps (OOOO0OOOOO0OOOOO0 )},headers ={'Api-Key':OO000O0O00OOOO0OO .api_key ,'Host-Url':frappe .request .headers ['Host']},timeout =10 )#line:40
-	if OO000OOO00O0OO00O .status_code ==200 and OO000OOO00O0OO00O .json ():#line:42
-		O00OOO00OOOOO0O00 =OO000OOO00O0OO00O .json ()['message']#line:43
-		if not O00OOO00OOOOO0O00 ['status']:#line:44
-			frappe .throw (O00OOO00OOOOO0O00 ['message'])#line:45
-		return O00OOO00OOOOO0O00 #line:46
-	return frappe .throw (f"status_code: {OO000OOO00O0OO00O.status_code}, there is an issue in the server")#line:48
+	OOO00OO00OOO00OO0 =[OOOOOO00OO00OO000 .component for OOOOOO00OO00OO000 in OOOOO0O0O00O00O0O .non_taxable_deductions ]#line:35
+	OO0O000O00O0000O0 =requests .post (pinger ,json ={'doc':OOOOOO00000O000O0 ,'non_taxable_keys':json .dumps (OOO00OO00OOO00OO0 )},headers ={'Api-Key':OOOOO0O0O00O00O0O .api_key ,'Host-Url':frappe .request .headers ['Host']},timeout =10 )#line:40
+	if OO0O000O00O0000O0 .status_code ==200 and OO0O000O00O0000O0 .json ():#line:42
+		OOOO000OOOOO0O00O =OO0O000O00O0000O0 .json ()['message']#line:43
+		if not OOOO000OOOOO0O00O ['status']:#line:44
+			frappe .throw (OOOO000OOOOO0O00O ['message'])#line:45
+		return OOOO000OOOOO0O00O #line:46
+	return frappe .throw (f"status_code: {OO0O000O00O0000O0.status_code}, there is an issue in the server")#line:48
 @frappe .whitelist ()#line:52
-def check_compute_payee (O0O0OOOOO00OOOOO0 ):#line:53
-	if type (O0O0OOOOO00OOOOO0 )==str :#line:54
-		O0O0OOOOO00OOOOO0 =frappe ._dict (json .loads (O0O0OOOOO00OOOOO0 ))#line:55
-	OOOOOO00O0O0OOO0O =frappe .get_doc ("Employee",O0O0OOOOO00OOOOO0 .employee ).as_dict ()#line:56
-	O0OOO0O00OO000O0O =getdate (O0O0OOOOO00OOOOO0 .start_date )#line:58
-	OOOOOOOOOOOOO0OO0 =(OOOOOO00O0O0OOO0O .date_of_joining if OOOOOO00O0O0OOO0O .date_of_joining >O0OOO0O00OO000O0O else O0OOO0O00OO000O0O )#line:61
-	O0000O00OO0000OOO =frappe .get_value ("Salary Structure Assignment",{"employee":O0O0OOOOO00OOOOO0 .employee ,"salary_structure":O0O0OOOOO00OOOOO0 .salary_structure ,"from_date":("<=",OOOOOOOOOOOOO0OO0 ),"docstatus":1 ,},"*",order_by ="from_date desc",as_dict =True ,)#line:73
-	if O0000O00OO0000OOO .compute_payee :#line:74
-		O0000000OOO00O000 =frappe .get_doc ('NGN PAYE Config')#line:75
-		return frappe ._dict (calculate_values (O0O0OOOOO00OOOOO0 ,O0000000OOO00O000 ))#line:76
-	return frappe ._dict ({'status':False })#line:77
+def check_compute_payee (OO000O00OOO00OOOO ):#line:53
+	if type (OO000O00OOO00OOOO )==str :#line:55
+		OO000O00OOO00OOOO =frappe ._dict (json .loads (OO000O00OOO00OOOO ))#line:56
+	OO0000O0O0OO0OOO0 =frappe .get_doc ("Employee",OO000O00OOO00OOOO .employee ).as_dict ()#line:57
+	O0O00OO00000O0OOO =getdate (OO000O00OOO00OOOO .start_date )#line:59
+	OO00OO0O000OOOOOO =(OO0000O0O0OO0OOO0 .date_of_joining if OO0000O0O0OO0OOO0 .date_of_joining >O0O00OO00000O0OOO else O0O00OO00000O0OOO )#line:62
+	OOOOOO0OOOO0OOOO0 =frappe .get_value ("Salary Structure Assignment",{"employee":OO000O00OOO00OOOO .employee ,"salary_structure":OO000O00OOO00OOOO .salary_structure ,"from_date":("<=",OO00OO0O000OOOOOO ),"docstatus":1 ,},"*",order_by ="from_date desc",as_dict =True ,)#line:74
+	if OOOOOO0OOOO0OOOO0 .compute_payee :#line:75
+		O000OOOOO00O00O00 =frappe .get_doc ('NGN PAYE Config')#line:76
+		return frappe ._dict (calculate_values (OO000O00OOO00OOOO ,O000OOOOO00O00O00 ))#line:77
+	return frappe ._dict ({'status':False })#line:78
